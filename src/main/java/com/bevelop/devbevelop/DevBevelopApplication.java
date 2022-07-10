ce3d0f2ffa6417814b8f62613f0c571ad510d817
hajihong <hajihong96@gmail.com>
2022-07-10 22:34:47 +0900

    init setting

---
+++ src/main/java/com/bevelop/devbevelop/DevBevelopApplication.java
@@ -0,0 +1,13 @@
+package com.bevelop.devbevelop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevBevelopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevBevelopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bevelop/devbevelop/DevBevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bevelop.devbevelop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevBevelopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
