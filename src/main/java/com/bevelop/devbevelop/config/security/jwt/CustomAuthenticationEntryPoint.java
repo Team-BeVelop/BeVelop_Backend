b55f0b19a70fe5e6ffcd2263e7bfe1eac23cebf3
hajihong <hajihong96@gmail.com>
2022-07-22 18:32:51 +0900

    Feat/ Jwt, Redis

---
+++ src/main/java/com/bevelop/devbevelop/config/security/jwt/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,32 @@
+package com.bevelop.devbevelop.config.security.jwt;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * AuthenticationEntryPoint
+ *
+ * 인증 과정에서 실패하거나 인증을 위한 헤더정보를 보내지 않은 경우
+ * 401(UnAuthorized) 에러가 발생하게 된다.
+ *
+ * Spring Security에서 인증되지 않은 사용자에 대한 접근 처리는 AuthenticationEntryPoint가 담당하는데,
+ * commence 메소드가 실행되어 처리된다.
+ */
+@Slf4j
+@Component
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    // 유효한 자격증명을 제공하지 않고 접근하려 할때 401
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException e) throws IOException {
+        System.out.println(request.getRequestURI());
+        log.error("UnAuthorized -- message : " + e.getMessage()); // 로그를 남기고
+        response.sendRedirect("/api/users/login"); // 로그인 페이지로 리다이렉트되도록 하였다.
+    }
+}
