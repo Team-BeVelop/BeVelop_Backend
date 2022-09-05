d63b1fbcd9518e28c2bebc597c5115a079c6678f
hajihong <hajihong96@gmail.com>
2022-09-06 01:35:43 +0900

    Feat/ 스터디 모집직무 추가

---
+++ src/main/java/com/bevelop/devbevelop/domain/study/query/data/RecruitJobData.java
@@ -0,0 +1,13 @@
+package com.bevelop.devbevelop.domain.study.query.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@ToString
+public class RecruitJobData {
+    private final Long id;
+    private final String jobName;
+}
