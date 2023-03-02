6e9f491689c63629604d7342cc6fd9fefe6f5309
juyoungpang <jenna990615@naver.com>
2023-03-02 09:28:42 +0900

    Revert "Refactoring --> header로 들어오는 토큰을 이용해 현재 사용자 정보를 받아오는 부분의 코드를 분리"
    This reverts commit 1dcd3a36af85bb8495d5c0f5cd98b4afca745dff.

--- src/main/java/com/bevelop/devbevelop/domain/user/controller/UserController.java
+++ src/main/java/com/bevelop/devbevelop/domain/user/controller/UserController.java
@@ -8,10 +8,14 @@ import com.bevelop.devbevelop.global.common.response.CommonResult;
 import com.bevelop.devbevelop.global.error.exception.CustomException;
 import com.bevelop.devbevelop.global.error.ErrorCode;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -28,16 +32,13 @@ public class UserController {
     public String hello() {
         return "testing Docker...";
     }
-    
-    private User getCurrentUser() {
-    	Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-    	return userService.findByEmail(authentication.getName()).orElseThrow(()->new CustomException(ErrorCode.MEMBER_NOT_FOUND));
-    }
 
     @ApiOperation(value = "현재 유저 프로필", notes = "현재 로그인된 유저의 정보")
     @GetMapping("/profile")
     public User profile() throws CustomException {
-        return getCurrentUser();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.findByEmail(authentication.getName()).orElseThrow(()-> new CustomException(ErrorCode.MEMBER_NOT_FOUND));
+        return user;
     }
 
     @ApiOperation(value = "유저 프로필 찾기", notes = "PathVariable로 주어진 username을 가진 유저의 정보")
@@ -52,8 +53,9 @@ public class UserController {
     @ApiOperation(value = "모든 유저", notes = "모든 유저의 리스트")
     @GetMapping("/userList")
     public List<User> showUserList() {
-    	User user = getCurrentUser();
-    	if(user.getRole().equals(Role.MASTER))
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.findByEmail(authentication.getName()).orElseThrow(()-> new CustomException(ErrorCode.MEMBER_NOT_FOUND));
+        if(user.getRole().equals(Role.MASTER))
             return userService.findAll();
         throw new CustomException(ErrorCode.NOT_MASTER);
     }
@@ -61,7 +63,8 @@ public class UserController {
     @ApiOperation(value = "유저 정보 수정", notes = "회원 수정")
     @PutMapping("/edit/{id}")
     public CommonResult updateUser(@Valid @RequestBody UserUpdateDto userUpdateDto) {
-        User user = getCurrentUser();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.findByEmail(authentication.getName()).orElseThrow(()-> new CustomException(ErrorCode.MEMBER_NOT_FOUND));
         return userService.updateUser(user, userUpdateDto);
     }
 }
